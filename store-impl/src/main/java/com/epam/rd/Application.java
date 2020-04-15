b59cee05c7e471c948e35c1c22c3b20182a672ec
ddmi1553 <dimanych112@mail.ru>
2020-04-15 11:28:45 +0400

    add Maven to project

---
+++ store-impl/src/main/java/com/epam/rd/Application.java
@@ -0,0 +1,11 @@
+package com.epam.rd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) {
+        log.info("Message");
+    }
+}
