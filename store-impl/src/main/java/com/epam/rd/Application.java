6ae150025dc29371768d1a79bbdc2c5c2dba2feb
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-04-15 17:46:33 +0400

    Add three Maven modules, add properties, plugins and dependencies manager on root module, also add on store-api module links to dependencies by root module. Configure logback.xml and logger. Remove java directory on db module. On impl module create package com.epam.rd, on this package was created Application.java with psvm

---
+++ store-impl/src/main/java/com/epam/rd/Application.java
@@ -0,0 +1,12 @@
+package com.epam.rd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        log.info("Hello world");
+    }
+}
