9876419df4a4c4918099641c3c97e87df793b3f6
AntonBelousov2020 <anton-belousov-2017@yandex.ru>
2020-04-23 11:48:34 +0400

    Add sql queries for create database, fix

--- store-db/resources/database.sql
+++ store-db/resources/database.sql
@@ -1,12 +1,12 @@
 drop schema if exists store cascade;
 
--- Инициализируем схему store
+-- РРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј СЃС…РµРјСѓ store
 create schema store;
 
--- Инициалтзируем таблицы, описанные в физической модели.
--- У таблиц должен быть префикс "st_".
+-- РРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј РјРѕРґРµР»Рё, РѕРїРёСЃР°РЅРЅС‹Рµ РІ С„РёР·РёС‡РµСЃРєРѕР№ РјРѕРґРµР»Рё.
+-- РЈ С‚Р°Р±Р»РёС† РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ РїСЂРµС„РёРєСЃ "st_".
 
--- Создадим таблицу st_product
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_product
 create table store.st_product (
 	id uuid primary key not null,
 	name varchar not null, 
@@ -14,14 +14,14 @@ create table store.st_product (
 	external_product_id uuid not null
 );
 
--- Создадим таблицу st_catalog
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_catalog
 create table store.st_catalog (
 	id uuid primary key not null,
 	product_id uuid references store.st_product(id) not null,
 	quantity int8 not null
 );
 
--- Создадим таблицу st_client_order
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_client_order
 create table store.st_client_order (
 	id uuid primary key not null,
 	amount int8 not null,
@@ -29,15 +29,15 @@ create table store.st_client_order (
 	payment_id uuid not null
 );
 
--- Создадим таблицу st_client_order_items
-create table store.st_clisent_order_items (
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_client_order_items
+create table store.st_client_order_items (
 	id uuid primary key not null,
 	product_id uuid references store.st_product(id) not null,
 	order_id uuid references store.st_client_order(id) not null,
 	quantity int8 not null
 );
 
--- Создадим таблицу st_supplier_order
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_supplier_order
 create table store.st_supplier_order (
 	id uuid primary key not null,
 	amount int8 not null,
@@ -46,7 +46,7 @@ create table store.st_supplier_order (
 	payment_id uuid not null
 );
 
--- Создадим таблицу st_supplier_order_items
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_supplier_order_items
 create table store.st_supplier_order_items (
 	id uuid primary key not null,
 	product_id uuid references store.st_product(id) not null,
@@ -54,7 +54,7 @@ create table store.st_supplier_order_items (
 	quantity int8 not null
 );
 
--- Создадим таблицу st_payment
+-- РЎРѕР·РґР°РґРёРј С‚Р°Р±Р»РёС†Сѓ st_payment
 create table store.st_payment (
 	id uuid primary key not null,
 	orgn_shop varchar(13) not null,
@@ -73,56 +73,55 @@ create table store.st_payment (
 	client_order_id uuid not null
 );
 
--- Добавим внешние ключи в таблицы st_supplier_order и st_payment
+-- Р”РѕР±Р°РІРёРј РІРЅРµС€РЅРёРµ РєР»СЋС‡Рё РІ С‚Р°Р±Р»РёС†Сѓ st_payment
 alter table store.st_payment
 add foreign key (supplier_order_id)
 references store.st_supplier_order(id);
 
--- Добавим внешние ключи в таблицы st_client_order и st_payment
 alter table store.st_payment
 add foreign key (client_order_id)
 references store.st_client_order(id);
 
--- Инициализируем пользователя store_user с одноименным паролем и следующими правами на схему store:
+-- РРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ store_user СЃ РѕРґРЅРѕРёРјРµРЅРЅС‹Рј РїР°СЂРѕР»РµРј Рё СЃР»РµРґСѓСЋС‰РёРјРё РїСЂР°РІР°РјРё РЅР° СЃС…РµРјСѓ store:
 -- select, insert, update, delete, truncate
--- использовать sequences
--- запуск functions
+-- РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ sequences
+-- Р·Р°РїСѓСЃРє functions
 select * from pg_shadow;
 
--- Создадим пользователя
+-- РЎРѕР·РґР°РґРёРј РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
 create user store_user with password 'store_user';
 
--- Добавим ему права select, insert, update, delete, truncate на схему store
+-- Р”РѕР±Р°РІРёРј РµРјСѓ РїСЂР°РІР° select, insert, update, delete, truncate РЅР° СЃС…РµРјСѓ store
 grant select, insert, update, delete, truncate on all tables in schema store to store_user;
 
--- Добавим ему права на использование sequences в схеме store
+-- Р”РѕР±Р°РІРёРј РµРјСѓ РїСЂР°РІР° РЅР° sequences РІ СЃС…РµРјСѓ store
 grant all privileges on all sequences in schema store to store_user;
 
--- Добавим ему права на запуск functions в схеме store
+-- Р”РѕР±Р°РІРёРј РµРјСѓ РїСЂР°РІР° РЅР° Р·Р°РїСѓСЃРє functions РІ СЃС…РµРјРµ store
 grant all privileges on all functions in schema store to store_user;
 
--- Добавим стабовые данные для проверки
+-- Р”РѕР±Р°РІРёРј СЃС‚Р°Р±РѕРІС‹Рµ РґР°РЅРЅС‹Рµ РґР»СЏ РїСЂРѕРІРµСЂРєРё
 insert into store.st_product (id, name, description, external_product_id) values
-('c12ab749-87bc-4099-adc4-f17e2c1d6e5a', 'Куртка', 'Верхняя одежда', '2e8e29dc-a44b-4a6a-851f-3d7e917d2b42'),
-('f0756d4b-34c6-484c-9fc5-71a0f1631c8e', 'Ботинки', 'Обувь', '64529260-4b60-4410-a47a-78f69cb36825');
+('c12ab749-87bc-4099-adc4-f17e2c1d6e5a', 'РљСѓСЂС‚РєР°', 'Р’РµСЂС…РЅСЏСЏ РѕРґРµР¶РґР°', '2e8e29dc-a44b-4a6a-851f-3d7e917d2b42'),
+('f0756d4b-34c6-484c-9fc5-71a0f1631c8e', 'Р‘РѕС‚РёРЅРєРё', 'РћР±СѓРІСЊ', '64529260-4b60-4410-a47a-78f69cb36825');
 
 insert into store.st_catalog (id, product_id, quantity) values
 ('950a02cd-5d49-4987-94a5-18bb6cef3f94', 'c12ab749-87bc-4099-adc4-f17e2c1d6e5a','5'),
 ('e992f376-c566-4204-9ce2-e635eb28801c','f0756d4b-34c6-484c-9fc5-71a0f1631c8e','8');
 
 insert into store.st_client_order (id, amount, status, payment_id) values
-('1f452b44-35e7-4743-8aed-0908de6c10db', '100', 'Готов', 'd85aa00f-b832-40cd-bd6e-6bb4092bb91d'),
-('cd0a874c-68ae-44b7-a600-11a1f7e41ec7', '200', 'В процессе', '2e5c9652-03b2-448b-9479-d1118f8c2239');
+('1f452b44-35e7-4743-8aed-0908de6c10db', '100', 'Р“РѕС‚РѕРІ', 'd85aa00f-b832-40cd-bd6e-6bb4092bb91d'),
+('cd0a874c-68ae-44b7-a600-11a1f7e41ec7', '200', 'Р’ РїСЂРѕС†РµСЃСЃРµ', '2e5c9652-03b2-448b-9479-d1118f8c2239');
 
-insert into store.st_clisent_order_items (id, product_id, order_id, quantity) values
+insert into store.st_client_order_items (id, product_id, order_id, quantity) values
 ('07b41a91-ec23-4d96-a160-8e053ccc137d', 'c12ab749-87bc-4099-adc4-f17e2c1d6e5a',
 '1f452b44-35e7-4743-8aed-0908de6c10db', '10'),
 ('acfecc60-80af-4678-a6b9-9eb9b5b8cee1', 'f0756d4b-34c6-484c-9fc5-71a0f1631c8e',
 'cd0a874c-68ae-44b7-a600-11a1f7e41ec7', '15');
 
 insert into store.st_supplier_order (id, amount, status, payment_callback_url, payment_id) values
-('1a5ce931-bad3-45c5-a4ca-d22a6fd9c453', '500','В процессе', 'https://jacket.com', 'd85aa00f-b832-40cd-bd6e-6bb4092bb91d'),
-('8b592226-df75-4609-8ca2-bb2af4d98b18', '100','Готов', 'https://shoes.com', '2e5c9652-03b2-448b-9479-d1118f8c2239');
+('1a5ce931-bad3-45c5-a4ca-d22a6fd9c453', '500','Р’ РїСЂРѕС†РµСЃСЃРµ', 'https://jacket.com', 'd85aa00f-b832-40cd-bd6e-6bb4092bb91d'),
+('8b592226-df75-4609-8ca2-bb2af4d98b18', '100','Р“РѕС‚РѕРІ', 'https://shoes.com', '2e5c9652-03b2-448b-9479-d1118f8c2239');
 
 insert into store.st_supplier_order_items (id, product_id, order_id, quantity) values
 ('7344f860-1b4a-4fb3-ad4c-dc2981733bff', 'c12ab749-87bc-4099-adc4-f17e2c1d6e5a',
@@ -135,7 +134,7 @@ orgn_client, kpp_client, inn_client, payment_account_client,
 key, amount, callback_url, status, supplier_order_id, client_order_id) values
 ('d85aa00f-b832-40cd-bd6e-6bb4092bb91d', '5044687244080', '929945885', '704541383710', '1111',
 '1085352563933', '711201844', '044906427958', '3333',
-'1234', '200', 'https://t-shot.com', 'Не оплачен', '1a5ce931-bad3-45c5-a4ca-d22a6fd9c453', '1f452b44-35e7-4743-8aed-0908de6c10db'),
+'1234', '200', 'https://t-shot.com', 'РќРµ РѕРїР»Р°С‡РµРЅ', '1a5ce931-bad3-45c5-a4ca-d22a6fd9c453', '1f452b44-35e7-4743-8aed-0908de6c10db'),
 ('2e5c9652-03b2-448b-9479-d1118f8c2239', '1177221583736', '730301089', '174868163911', '2222',
 '1068652878139', '579944516', '497171148858', '4444',
-'4321', '100', 'https://trainings.com', 'Оплачен', '8b592226-df75-4609-8ca2-bb2af4d98b18', 'cd0a874c-68ae-44b7-a600-11a1f7e41ec7');
+'4321', '100', 'https://trainings.com', 'РћРїР»Р°С‡РµРЅ', '8b592226-df75-4609-8ca2-bb2af4d98b18', 'cd0a874c-68ae-44b7-a600-11a1f7e41ec7');
