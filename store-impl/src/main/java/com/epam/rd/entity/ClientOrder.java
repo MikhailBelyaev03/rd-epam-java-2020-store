45aa3f73343ac388a13734b7b9e24438933bcd2b
ShkrylAndrei <ShkrylAndrei@rambler.ru>
2020-05-01 10:27:53 +0400

    add ClientOrder entities and Repositories (#35)

--- store-impl/src/main/java/com/epam/rd/entity/ClientOrder.java
+++ store-impl/src/main/java/com/epam/rd/entity/ClientOrder.java
@@ -1,5 +1,39 @@
 package com.epam.rd.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "st_client_order")
+@NoArgsConstructor
+@Data
 public class ClientOrder {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    private long amount;
+
+    private String status;
+
+    private UUID payment_id;
+
+    @OneToMany(mappedBy = "clientOrder", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<ClientOrderItem> clientOrderItem;
 
 }
--- store-impl/src/main/java/com/epam/rd/entity/ClientOrderItem.java
+++ store-impl/src/main/java/com/epam/rd/entity/ClientOrderItem.java
@@ -1,5 +1,38 @@
 package com.epam.rd.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.UUID;
+
+@Entity
+@Table(name = "st_client_order_items")
+@NoArgsConstructor
+@Data
 public class ClientOrderItem {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private long quantity;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private ClientOrder clientOrder;
 }
