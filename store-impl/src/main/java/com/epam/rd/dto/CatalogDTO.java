da90481b57624a67ef47a6e561163783654cc51d
ShkrylAndrei <shkrylandrei@rambler.ru>
2020-07-07 10:16:43 +0400

    create catalog

---
+++ store-impl/src/main/java/com/epam/rd/dto/CatalogDTO.java
@@ -0,0 +1,13 @@
+package com.epam.rd.dto;
+
+import com.epam.rd.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogDTO {
+
+    public List<Product> products  = new ArrayList<>();
+
+    public Product product;
+}
